00a4a643a32169bf881df13e7d919fb6f61061b6
Adenekan123 <mr.nekan123@gmail.com>
2023-04-07 22:33:54 +0100

    career update

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
-import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Aside } from "../components/Aside";
 import { Main } from "../components/Main";

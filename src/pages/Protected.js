7bd6b22f261b738fb447d3cdcf7e8fde0701d124
Adenekan123 <mr.nekan123@gmail.com>
2023-04-20 07:17:58 +0100

    added date to farmvist preview

--- src/pages/Protected.js
+++ src/pages/Protected.js
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 function Protected({ children }) {
   const navigate = useNavigate();
-  const { isAuthenticated } = useSelector((store) => store.auth);
-  // const isAuthenticated = localStorage.getItem("mktoken");
+  // const { isAuthenticated } = useSelector((store) => store.auth);
+  const isAuthenticated = localStorage.getItem("mktoken");
   useEffect(() => {
     if (!isAuthenticated) return navigate("/login");
   }, [isAuthenticated, navigate]);
